4b292228c2ae283e4bc02d6d61f7b7d4d8933f29
msaisushma <msaisushma@zeomega.com>
2014-06-20 17:25:42 +0530

    Update pyapi.py

--- pyapi.py
+++ pyapi.py
@@ -3,19 +3,12 @@ import json
 
 Access_token = 'b0e86264e1b3dfb6d8f93c84eda09c2e42c82017'
 api_url = "https://api.github.com/users"
-usrname = raw_input("Enter the username:")
-api_url = "https://api.github.com/users/"
 
-def get_authorization():
-	url = api_url+usrname
-	if auth:
-		return request.get(url,headers={'Authorization': ' '.join(['token',Access_token])})
-	return request.get(url)
 
 def get_comments():
 	usrname = raw_input("Enter the username:")
 	url = api_url+usrname+"/events"
-	req = requests.get(url,auth=True)
+	req = requests.get(url)
 	res = req.content
 	# print res
 	resp = json.loads(res)
