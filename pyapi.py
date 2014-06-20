e0053e67baed62b9aaad02451edf906f0a10cb19
Saisushma <msaisushma@zeomega.com>
2014-06-20 17:21:53 +0530

    This prints commitcomment url for an username like msaisushma

---
+++ pyapi.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+Access_token = 'b0e86264e1b3dfb6d8f93c84eda09c2e42c82017'
+api_url = "https://api.github.com/users"
+usrname = raw_input("Enter the username:")
+api_url = "https://api.github.com/users/"
+
+def get_authorization():
+	url = api_url+usrname
+	if auth:
+		return request.get(url,headers={'Authorization': ' '.join(['token',Access_token])})
+	return request.get(url)
+
+def get_comments():
+	usrname = raw_input("Enter the username:")
+	url = api_url+usrname+"/events"
+	req = requests.get(url,auth=True)
+	res = req.content
+	# print res
+	resp = json.loads(res)
+
+	pload = [li['payload']for li in resp]
+	payload = pload[0]
+	# print payload
+
+	for k,v in payload.items():
+		print v['html_url']
+
+if __name__ == '__main__':
+	get_comments()
